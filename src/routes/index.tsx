65a9d007f8929e25184c4bc0b9d1ceaa5676bcce
sovietrat <67848010+sovietrat@users.noreply.github.com>
2023-02-22 19:43:42 -0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,10 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
+      <Title>Soviet</Title>
+      <h1>Welcome</h1>
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        Welcome to Soviet's official website@
       </p>
     </main>
   );
